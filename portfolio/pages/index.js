299d1896b015875512af6f0d03ca41a51e44c308
Anisha Hirani <anisha.hirani11@gmail.com>
2023-03-10 16:46:42 +0000

    starting working with navbar and react icons

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -1,4 +1,5 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import {BsFillMoonStarsFill} from "react-icons/bs";
 
 export default function Home() {
   return (
@@ -10,7 +11,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
-      <main></main>
+      <main>
+        <section className="min-h-screen">
+          <nav>
+          <h1>developedbyAnisha</h1>
+          <ul>
+            <li><BsFillMoonStarsFill/></li>
+            <lli><a href="#">Resume</a></lli>
+          </ul>
+          </nav>
+        </section>
+      </main>
     </div>
   )
 }
