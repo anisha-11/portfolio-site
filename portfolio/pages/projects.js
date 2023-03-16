5863cd8df5e9b4acbe907a81c555e004cf56a5dc
Anisha Hirani <anisha.hirani11@gmail.com>
2023-03-16 17:33:48 +0000

    moved projects over from index.js and applied a margin-top

--- portfolio/pages/projects.js
+++ portfolio/pages/projects.js
@@ -17,29 +17,51 @@ export default function Projects() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <section>
-          <navbar>
-            <h1>
-              <ul>
-              <li>
-                <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='ml-8 cursor-pointer text-2xl'/>
-              </li>
-              <li>
-                <a className="bg-teal-600 text-white dark:bg-orange-200 dark:text-black px-4 py-2 rounded-md ml-8 mr-8" href="/Anisha Hirani - CV.pdf" target="_blank">Resume</a>
-              </li>
-              <li> 
-                <a className="bg-teal-600 text-white dark:bg-orange-200 dark:text-black px-4 py-2 rounded-md ml-0 mr-4" href="/about" target>About Me</a>
-              </li>
-              <li> 
-                <a className="bg-teal-600 text-white dark:bg-orange-200 dark:text-black px-4 py-2 rounded-md ml-0 mr-4" href="/" target>Home</a>
-              </li>
-              <li> 
-                <a className="bg-teal-600 text-white dark:bg-orange-200 dark:text-black px-4 py-2 rounded-md ml-0 mr-4" href="/" target>Projects</a>
-              </li>
+      <main className="bg-orange-100 px-0 md:px-20 lg:px-0 dark:bg-gray-700">
+        <section className="min-h-screen">
+          <navbar className="fixed w-full z-20 top-0 left-0 bottom-50 bg-gray-700 py-10 mb-9 flex justify-between dark:bg-teal-600">
+            <h1 className='text-xl text-teal-500 dark:text-orange-200 font-bold font-burtons ml-8 tracking-wider'>Developed by Anisha Hirani</h1>
+              <ul className="flex items-center">
+                <li>
+                  <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='ml-8 cursor-pointer text-2xl'/>
+                </li>
+                <li>
+                  <a className="bg-teal-600 text-white dark:bg-orange-200 dark:text-black px-4 py-2 rounded-md ml-8 mr-8" href="/Anisha Hirani - CV.pdf" target="_blank">Resume</a>
+                </li>
+                <li> 
+                  <a className="bg-teal-600 text-white dark:bg-orange-200 dark:text-black px-4 py-2 rounded-md ml-0 mr-4" href="/about" target>About Me</a>
+                </li>
+                <li> 
+                  <a className="bg-teal-600 text-white dark:bg-orange-200 dark:text-black px-4 py-2 rounded-md ml-0 mr-4" href="/" target>Home</a>
+                </li>
+                <li> 
+                  <a className="bg-teal-600 text-white dark:bg-orange-200 dark:text-black px-4 py-2 rounded-md ml-0 mr-4" href="/" target>Projects</a>
+                </li>
               </ul>
-            </h1>
           </navbar>
+          <section className="flex justify-center align-center text-center flex-wrap:wrap items-stretch">
+            <div className='flex-col justify-center items-center ml-auto mr-auto max-w-l"'>
+              <div className="text-center shadow-lg dark:bg-teal-600 bg-gray-700 shadow-gray-700 dark:shadow-teal-600 p-1.5 rounded-xl my-8 mr-12 ml-12 mt-40 mx-auto justify-center">
+                <Image className="border-solid border-2 dark:border-black rounded-xl" src={reelplaces} width={650} height={200}/>
+                <h3 className="text-4xl text-teal-500 dark:text-orange-200 font-extrabold pt-8 pb-2">REELPLACES</h3>
+                <p className="text-md py-5 leading-5 text-white dark:text-white md:text-xl max-w-xl mx-auto">
+                  Makers final project! A web-application that allows you to search for a location 
+                  you are currently 
+                  at and shows you the top 20 movies filmed in that particular location 
+                  with a map that pinpoints that location. </p>
+              </div>
+              <div className="text-center bg-gray-700 dark:bg-teal-600 shadow-lg shadow-gray-700 dark:shadow-teal-600 p-1.5 rounded-xl my-8 mr-12 ml-12 mx-auto justify-center">
+                <Image className="border-solid border-2 dark:border-black rounded-xl" src={acebook} width={650} height={200} />
+                <h3 className="text-4xl text-teal-500 dark:text-orange-200 font-extrabold pt-8 pb-2">ACEBOOK</h3>
+                <p className="text-md py-5 leading-8 text-white dark:text-white md:text-xl max-w-xl mx-auto">A clone of Facebook using the MERN Stack and allowing users to comment, upload pictures and have fun on this social media platform.</p>
+              </div>
+              <div className="text-center display-flex dark:bg-teal-600 shadow-lg bg-gray-700 shadow-gray-700 dark:shadow-teal-600 p-1.5 rounded-xl my-8 mr-12 ml-12 mx-auto justify-center">
+                <Image className="border-solid border-2 dark:border-black rounded-xl" src={bnb} width={650} height={200} />
+                <h3 className="text-4xl  text-teal-500 dark:text-orange-200 font-extrabold pt-8 pb-2">MAKERS-BNB</h3>
+                <p className="text-md py-5 leading-8 text-white dark:text-white md:text-xl max-w-xl mx-auto">First ever group project with Makers which was a clone of AirBnB using Ruby, Sinatra, databases and much more.</p>
+              </div>
+            </div>
+          </section>
         </section>
       </main>
     </div>
