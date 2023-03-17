b641d7c667e988337d6ea595deefae5b2916283d
Anisha Hirani <anisha.hirani11@gmail.com>
2023-03-17 15:40:17 +0000

    updated dark mode color changes and added about button on navbar

--- portfolio/pages/projects.js
+++ portfolio/pages/projects.js
@@ -23,8 +23,8 @@ export default function Projects() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-orange-100 px-0 md:px-20 lg:px-0 dark:bg-gray-700">
-        <navbar className="fixed w-full z-20 top-0 left-0 bottom-50 bg-gray-700 py-10 mb-9 flex justify-between dark:bg-teal-600">
+      <main className="bg-orange-100 px-0 md:px-20 lg:px-0 dark:bg-teal-600">
+        <navbar className="fixed w-full z-20 top-0 left-0 bottom-50 bg-gray-700 py-10 mb-9 flex justify-between dark:bg-gray-700">
           <h1 className='text-xl text-teal-500 dark:text-orange-200 font-bold font-burtons ml-8 tracking-wider'>Developed by Anisha Hirani</h1>
             <ul className="flex items-center">
               <li>
@@ -34,7 +34,7 @@ export default function Projects() {
                 <a className="bg-teal-600 text-white dark:bg-orange-200 dark:text-black px-4 py-2 rounded-md ml-8 mr-8" href="/Anisha Hirani - CV.pdf" target="_blank">Resume</a>
               </li>
               <li> 
-                <a className="bg-teal-600 text-white dark:bg-orange-200 dark:text-black px-4 py-2 rounded-md ml-0 mr-4" href="/about" target>About Me</a>
+                <a className="bg-teal-600 text-white dark:bg-orange-200 dark:text-black px-4 py-2 rounded-md ml-0 mr-4" href="/about" target>About</a>
               </li>
               <li> 
                 <a className="bg-teal-600 text-white dark:bg-orange-200 dark:text-black px-4 py-2 rounded-md ml-0 mr-4" href="/" target>Home</a>
@@ -51,7 +51,7 @@ export default function Projects() {
         </section>
         <section className="flex flex-wrap:wrap justify-center align-center text-center flex-wrap:wrap items-stretch">
             <div className='grid grid-cols-3 justify-center items-center ml-auto mr-auto max-w-l"'>
-              <div className="text-center shadow-lg dark:bg-teal-600 bg-gray-700 shadow-gray-700 dark:shadow-teal-600 p-1.5 rounded-xl my-8 mr-12 ml-12 mx-auto justify-center">
+              <div className="text-center shadow-lg dark:bg-gray-700 bg-gray-700 shadow-gray-700 dark:shadow-gray-700 p-1.5 rounded-xl my-8 mr-12 ml-12 mx-auto justify-center">
                 <Image className="border-solid border-2 dark:border-black rounded-xl" src={reelplaces} width={650} height={200}/>
                 <h3 className="text-3xl text-teal-500 dark:text-orange-200 font-extrabold pt-8 pb-2">REELPLACES</h3>
                 <p className="text-md py-5 leading-8 text-white dark:text-white md:text-md max-w-xl mx-auto">
@@ -63,7 +63,7 @@ export default function Projects() {
                   <a className='hover:text-orange-100' href={reelplacesLink} target="_blank"><HiOutlineLink /></a>
                 </div>
               </div>
-              <div className="text-center bg-gray-700 dark:bg-teal-600 shadow-lg shadow-gray-700 dark:shadow-teal-600 p-1.5 rounded-xl my-8 mr-12 ml-12 mx-auto justify-center">
+              <div className="text-center bg-gray-700 dark:dark:bg-gray-700 shadow-lg shadow-gray-700 dark:shadow-gray-700 p-1.5 rounded-xl my-8 mr-12 ml-12 mx-auto justify-center">
                 <Image className="border-solid border-2 dark:border-black rounded-xl" src={acebook} width={650} height={200} />
                 <h3 className="text-3xl text-teal-500 dark:text-orange-200 font-extrabold pt-8 pb-2">ACEBOOK</h3>
                 <p className="text-md py-5 leading-8 text-white dark:text-white md:text-md max-w-xl mx-auto">Similar to Facebook using the MERN Stack and allowing users to comment, upload pictures and have fun on this social media platform.</p>
@@ -71,7 +71,7 @@ export default function Projects() {
                   <a className='hover:text-orange-100' href={acebookRepo} target="_blank"><AiFillGithub /></a>
                 </div>
                 </div>
-              <div className="text-center display-flex dark:bg-teal-600 shadow-lg bg-gray-700 shadow-gray-700 dark:shadow-teal-600 p-1.5 rounded-xl my-8 mr-12 ml-12 mx-auto justify-center">
+              <div className="text-center display-flex dark:bg-gray-700 shadow-lg bg-gray-700 shadow-gray-700 dark:shadow-gray-700 p-1.5 rounded-xl my-8 mr-12 ml-12 mx-auto justify-center">
                 <Image className="border-solid border-2 dark:border-black rounded-xl" src={bnb} width={650} height={200} />
                 <h3 className="text-3xl  text-teal-500 dark:text-orange-200 font-extrabold pt-8 pb-2">MAKERS-BNB</h3>
                 <p className="text-md py-5 leading-8 text-white dark:text-white md:text-md max-w-md mx-auto">First group 
