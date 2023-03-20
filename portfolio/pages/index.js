198e5b188618896615050d818593c2ae3d942d12
Anisha Hirani <anisha.hirani11@gmail.com>
2023-03-20 10:32:23 +0000

    completed home page and edited navbar buttons

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -40,32 +40,32 @@ export default function Home() {
           <h1 className='text-xl text-teal-500 dark:text-orange-200 font-bold font-burtons ml-8 tracking-wider'>Developed by Anisha Hirani</h1>
           <ul className="flex items-center">
             <li>
-              <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='ml-8 cursor-pointer text-2xl'/>
+              <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='mr-5 cursor-pointer text-2xl'/>
             </li>
             <li>
-              <a className="bg-teal-600 hover:bg-teal-700 text-white dark:bg-orange-200 dark:hover:bg-orange-300 dark:text-black px-4 py-2 rounded-md ml-8 mr-8" href="/Anisha Hirani - CV.pdf" target="_blank">Resume</a>
+              <a className="bg-teal-600 hover:bg-teal-700 text-white dark:bg-orange-200 dark:hover:bg-orange-300 dark:text-black px-4 py-2 rounded-md ml-0 mr-4" href="/" target>Home</a>
             </li>
             <li> 
               <a className="bg-teal-600 hover:bg-teal-700 text-white dark:bg-orange-200 dark:hover:bg-orange-300 dark:text-black px-4 py-2 rounded-md ml-0 mr-4" href="/about" target>About</a>
             </li>
             <li> 
-              <a className="bg-teal-600 hover:bg-teal-700 text-white dark:bg-orange-200 dark:hover:bg-orange-300 dark:text-black px-4 py-2 rounded-md ml-0 mr-4" href="/" target>Home</a>
+              <a className="bg-teal-600 hover:bg-teal-700 text-white dark:bg-orange-200 dark:hover:bg-orange-300 dark:text-black px-4 py-2 rounded-md ml-0 mr-4" href="/projects" target>Projects</a>
             </li>
             <li> 
-              <a className="bg-teal-600 hover:bg-teal-700 text-white dark:bg-orange-200 dark:hover:bg-orange-300 dark:text-black px-4 py-2 rounded-md ml-0 mr-4" href="/projects" target>Projects</a>
+              <a className="bg-teal-600 hover:bg-teal-700 text-white dark:bg-orange-200 dark:hover:bg-orange-300 dark:text-black px-4 py-2 rounded-md ml-0 mr-4" href="/Anisha Hirani - CV.pdf" target="_blank">Resume</a>
             </li>
           </ul>
           </navbar>
           <div className="text-center p-10 py-10">
-            <h2 className="h-20 text-5xl py-20 mt-0 text-teal-600 dark:text-orange-200 font-medium">Anisha Hirani</h2>
-            <h3 className="text-2xl py-2 dark:text-white md:text-3xl">Developer and designer.</h3>
+            <h2 className="h-20 text-5xl py-20 mt-20 text-teal-600 dark:text-orange-200 font-bold">Anisha Hirani</h2>
+            <h3 className="text-2xl py-2 mt-4 dark:text-white md:text-3xl">Software developer and designer & ex Maths and English Tutor.</h3>
             {/* <p className="text-md py-5 leading-8 text-black dark:text-white md:text-xl max-w-lg mx-auto">
               Makers Academy graduate that is passionate about working in the tech industry in 
               order to make a positive impact on the future of technology for the society.
             </p> */}
           </div>
           <div className="flex justify-between ml-18 text-center text-5xl py-3 text-teal-600 dark:text-orange-200 md:text-6xl">
-            <div className="flex gap-5 justify-between ml-auto mt-32 mr-auto max-w-l">
+            <div className="flex gap-5 justify-between ml-auto mt-5 mr-auto max-w-l">
             <div class="relative before:z-10 before:absolute before:left-1/2 before:-top-3 before:w-max before:max-w-xs before:-translate-x-1/2 before:-translate-y-full before:rounded-lg before:bg-gray-700 dark:before:bg-teal-600 before:px-2 before:py-1.5 before:text-white before:text-sm before:invisible before:content-[attr(data-tip)] after:z-10 after:absolute after:left-1/2 after:-top-3 after:h-0 after:w-0 after:-translate-x-1/2 after:border-8 after:border-t-gray-700 after:border-l-transparent after:border-b-transparent after:border-r-transparent after:invisible hover:before:visible hover:after:visible" data-tip="JavaScript"><DiJsBadge/></div>
             <div class="relative before:z-10 before:absolute before:left-1/2 before:-top-3 before:w-max before:max-w-xs before:-translate-x-1/2 before:-translate-y-full before:rounded-lg before:bg-gray-700 dark:before:bg-teal-600 before:px-2 before:py-1.5 before:text-white before:text-sm before:invisible before:content-[attr(data-tip)] after:z-10 after:absolute after:left-1/2 after:-top-3 after:h-0 after:w-0 after:-translate-x-1/2 after:border-8 after:border-t-gray-700 after:border-l-transparent after:border-b-transparent after:border-r-transparent after:invisible hover:before:visible hover:after:visible" data-tip="React"><DiReact/></div>
             <div class="relative before:z-10 before:absolute before:left-1/2 before:-top-3 before:w-max before:max-w-xs before:-translate-x-1/2 before:-translate-y-full before:rounded-lg before:bg-gray-700 dark:before:bg-teal-600 before:px-2 before:py-1.5 before:text-white before:text-sm before:invisible before:content-[attr(data-tip)] after:z-10 after:absolute after:left-1/2 after:-top-3 after:h-0 after:w-0 after:-translate-x-1/2 after:border-8 after:border-t-gray-700 after:border-l-transparent after:border-b-transparent after:border-r-transparent after:invisible hover:before:visible hover:after:visible" data-tip="Ruby"><DiRuby/></div>
